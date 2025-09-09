15708bc48777b1800b69d3c6f2ac69d078a90109
Jorgeagutierrezm <joagutierrezm@udistrital.edu.co>
2025-09-09 13:13:08 -0500

    Update impares.py

--- corte1/logica secuencial/impares.py
+++ corte1/logica secuencial/impares.py
@@ -1,15 +1,4 @@
 #Jorge Andres Gutierrez este ejercicio vale por una decima del corte.
-#     residual = i%2
-#     if residual == 0:
-#         print(f'{i} is even')
-#     else:
-#         #print(f'{i} is odd')
-#         print(str(i) + ' is odd')
-
-# for i in range (0,6):
-#     result = i**3
-#     print(result)
-
 times = input("Enter a number of times: ")
 times = int(float(times))  # Convertimos a float y luego a int para evitar errores
 print(type(times))
